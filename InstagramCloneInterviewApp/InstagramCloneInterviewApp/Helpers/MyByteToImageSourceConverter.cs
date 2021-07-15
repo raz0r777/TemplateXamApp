ffdf97080c6d76ef35a68dbd8594f1f234a6c45b
Vasko Josifovski <vasko.josifovski@wsa.com>
2021-07-15 14:39:03 +0200

    added converter

---
+++ InstagramCloneInterviewApp/InstagramCloneInterviewApp/Helpers/MyByteToImageSourceConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+namespace InstagramCloneInterviewApp.Helpers
+{
+    public class MyByteToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                Stream ImageData = (Stream)value;
+                ImageData.Position = 0;
+
+                return ImageSource.FromStream(() => ImageData);
+            }
+            else
+            {
+               return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
